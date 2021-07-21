8e09d2d62d99c20efaf1275731cb0f73990f86c5
Nahyun Cho <w2038@e-mirim.hs.kr>
2021-07-21 13:40:06 +0900

    Edit MainActivity.java Commit

--- app/src/main/java/kr/hs/emirim/w2038/progect_bmi/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/w2038/progect_bmi/MainActivity.java
@@ -2,13 +2,35 @@ package kr.hs.emirim.w2038.progect_bmi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText editw, edith;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editw = findViewById(R.id.edit_weight);
+        edith = findViewById(R.id.edit_height);
+
+        Button btnCheck = findViewById(R.id.btn_check);
+        btnCheck.setOnClickListener(btnListener);
     }
+
+    View.OnClickListener btnListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            double weight = Double.parseDouble(editw.getText().toString());
+            double height = Double.parseDouble(edith.getText().toString());
+            Intent intent = new Intent(getApplicationContext(), ResultActivity.class);
+            intent.putExtra("weight", weight);
+            intent.putExtra("height", height);
+            startActivity(intent);
+        }
+    };
 }
